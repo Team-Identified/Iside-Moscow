af0c0a8a1a3b07a7dbd000ec0c3730a23487aa3c
fedor <daldalron007@gmail.com>
2021-04-09 11:08:12 +0300

    added error messages

--- mobile_app/lib/pages/loginPage.dart
+++ mobile_app/lib/pages/loginPage.dart
@@ -5,15 +5,11 @@ import 'package:mobile_app/config.dart';
 import 'package:mobile_app/components/Button.dart';
 import 'package:mobile_app/components/AlreadyHaveAnAccount.dart';
 
-
 void displayDialog(context, title, text) => showDialog(
-  context: context,
-  builder: (context) =>
-      AlertDialog(
-          title: Text(title),
-          content: Text(text)
-      ),
-);
+      context: context,
+      builder: (context) =>
+          AlertDialog(title: Text(title), content: Text(text)),
+    );
 
 class LoginPage extends StatelessWidget {
   final TextEditingController _usernameController = TextEditingController();
@@ -21,71 +17,80 @@ class LoginPage extends StatelessWidget {
   final VoidCallback onSignUpButtonPressed;
   final VoidCallback onSubmitButtonPressed;
 
-  LoginPage({@required this.onSignUpButtonPressed, @required this.onSubmitButtonPressed});
+  LoginPage(
+      {@required this.onSignUpButtonPressed,
+      @required this.onSubmitButtonPressed});
 
   Future<Map> attemptLogIn(String username, String password) async {
-    var res = await http.post(
-        Uri.http(SERVER_URL, '/auth/jwt/create/'),
-        body: {
-          "username": username,
-          "password": password
-        }
-    );
-    if(res.statusCode == 200) return json.decode(res.body);
-    return null;
+    var res = await http.post(Uri.http(SERVER_URL, '/auth/jwt/create/'),
+        body: {"username": username, "password": password});
+    int statusCode = res.statusCode;
+    Map body = json.decode(res.body);
+    return {"statuscode": statusCode, "body": body};
   }
-
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              "LOGIN",
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 0.03),
-            TextField(
-              controller: _usernameController,
-              decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(20.0),
-                  border: InputBorder.none,
-                  hintText: 'Username'
-              ),
-            ),
-            SizedBox(height: 0.03),
-            TextField(
-              controller: _passwordController,
-              obscureText: true,
-              decoration: InputDecoration(
-                  contentPadding: EdgeInsets.all(20.0),
-                  border: InputBorder.none,
-                  hintText: 'Password'
-              ),
-            ),
-            Button(
-              text: "LOGIN",
-              press: () async {
-                String username = _usernameController.text;
-                String password = _passwordController.text;
-                Map jwt = await attemptLogIn(username, password);
-                if(jwt != null) {
-                  storage.write(key: "access_jwt", value: jwt["access"]);
-                  storage.write(key: "refresh_jwt", value: jwt["refresh"]);
-                  onSubmitButtonPressed();
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Text(
+            "LOGIN",
+            style: TextStyle(fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 0.03),
+          TextField(
+            controller: _usernameController,
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(20.0),
+                border: InputBorder.none,
+                hintText: 'Username'),
+          ),
+          SizedBox(height: 0.03),
+          TextField(
+            controller: _passwordController,
+            obscureText: true,
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(20.0),
+                border: InputBorder.none,
+                hintText: 'Password'),
+          ),
+          Button(
+            text: "LOGIN",
+            press: () async {
+              String username = _usernameController.text;
+              String password = _passwordController.text;
+              Map res = await attemptLogIn(username, password);
+              Map body = res["body"];
+              int statusCode = res["statuscode"];
+              String title = "Error";
+              if (statusCode >= 400) {
+                if (body.containsKey("username")) {
+                  displayDialog(context, title, "Your username can't be blank");
+                } else if (body.containsKey("password")) {
+                  displayDialog(context, title, "Your password can't be blank");
+                } else if (body.containsKey("detail")) {
+                  displayDialog(context, title, body["detail"]);
+                } else {
+                  displayDialog(context, title, "Unknown error happened");
                 }
-                },
-            ),
-            SizedBox(height: 0.03),
-            AlreadyHaveAnAccount(
-              press: () {
-                onSignUpButtonPressed();
-              },
-            ),
-          ],
-        ),
+              }
+              if (statusCode == 200) {
+                storage.write(key: "access_jwt", value: body["access"]);
+                storage.write(key: "refresh_jwt", value: body["refresh"]);
+                onSubmitButtonPressed();
+              }
+            },
+          ),
+          SizedBox(height: 0.03),
+          AlreadyHaveAnAccount(
+            press: () {
+              onSignUpButtonPressed();
+            },
+          ),
+        ],
+      ),
     );
   }
 }
--- mobile_app/lib/pages/signUpPage.dart
+++ mobile_app/lib/pages/signUpPage.dart
@@ -6,6 +6,7 @@ import 'package:mobile_app/pages/loginPage.dart';
 import 'package:mobile_app/components/Button.dart';
 import 'package:mobile_app/components/AlreadyHaveAnAccount.dart';
 
+import 'loginPage.dart';
 
 class SignUpPage extends StatelessWidget {
   final TextEditingController _emailController = TextEditingController();
@@ -15,16 +16,11 @@ class SignUpPage extends StatelessWidget {
 
   SignUpPage({@required this.onLogInButtonPressed});
 
-  Future<int> attemptSignUp(String email, String username, String password) async {
-    var res = await http.post(
-        Uri.http(SERVER_URL, '/auth/users/'),
-        body: {
-          "email": email,
-          "username": username,
-          "password": password
-        }
-    );
-    return res.statusCode;
+  Future<Map> attemptSignUp(
+      String email, String username, String password) async {
+    var res = await http.post(Uri.http(SERVER_URL, '/auth/users/'),
+        body: {"email": email, "username": username, "password": password});
+    return {"body": json.decode(res.body), "statuscode": res.statusCode};
   }
 
   @override
@@ -43,8 +39,7 @@ class SignUpPage extends StatelessWidget {
             decoration: InputDecoration(
                 contentPadding: EdgeInsets.all(20.0),
                 border: InputBorder.none,
-                hintText: 'E-mail'
-            ),
+                hintText: 'E-mail'),
           ),
           SizedBox(height: 0.03),
           TextField(
@@ -52,8 +47,7 @@ class SignUpPage extends StatelessWidget {
             decoration: InputDecoration(
                 contentPadding: EdgeInsets.all(20.0),
                 border: InputBorder.none,
-                hintText: 'Username'
-            ),
+                hintText: 'Username'),
           ),
           SizedBox(height: 0.03),
           TextField(
@@ -62,8 +56,7 @@ class SignUpPage extends StatelessWidget {
             decoration: InputDecoration(
                 contentPadding: EdgeInsets.all(20.0),
                 border: InputBorder.none,
-                hintText: 'Password'
-            ),
+                hintText: 'Password'),
           ),
           Button(
             text: "SIGN UP",
@@ -71,12 +64,26 @@ class SignUpPage extends StatelessWidget {
               String email = _emailController.text;
               String username = _usernameController.text;
               String password = _passwordController.text;
-              int res = await attemptSignUp(email, username, password);
-              if(res != 201){
-                displayDialog(context, "Error", "An unknown error occurred.");
-              }
-              else{
+              Map res = await attemptSignUp(email, username, password);
+              int statusCode = res["statuscode"];
+              Map body = res["body"];
+              String title = "Error";
+              if (statusCode == 201) {
                 onLogInButtonPressed();
+              }
+              else if(statusCode>=400) {
+                if (email.length == 0 || username.length == 0 || password.length == 0) {
+                    displayDialog(context, title, "Please fill in every field given");
+                }
+                else if (body.containsKey("email")) {
+                  displayDialog(context, title, body["email"][0]);
+                }
+                else if (body.containsKey("username")) {
+                  displayDialog(context, title, body["username"][0]);
+                }
+                else if (body.containsKey("password")) {
+                  displayDialog(context, title, body["password"][0]);
+                }
               }
             },
           ),
