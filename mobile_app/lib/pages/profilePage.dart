9a533575e87f2366fdd7a2ab2c11ee0abaa2fd55
fedor <daldalron007@gmail.com>
2021-04-13 00:26:28 +0300

    upgraded profile page part 1

--- mobile_app/lib/pages/profilePage.dart
+++ mobile_app/lib/pages/profilePage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobile_app/components/Button.dart';
 import 'package:mobile_app/config.dart';
 import 'package:mobile_app/tools.dart';
 import 'package:flutter/material.dart';
@@ -22,16 +23,16 @@ class ProfilePage extends StatefulWidget {
 
 class _ProfileState extends State<ProfilePage> {
   String name, email, rank, dateJoined, id;
-  int points;
+  String points;
   Future<void> getProfile() async {
-    var res_get_url = await serverRequest("get", "auth/users/me", null);
-    id = res_get_url["id"].toString();
-    var res = await serverRequest("get", "/accounts/profile/" + id, null);
-    var data = res["user"];
+    var resGetUrl = await serverRequest("get", "auth/users/me", null);
+    id = resGetUrl["id"].toString();
+    Map res = await serverRequest("get", "accounts/profile/" + id, null);
+    Map data = res["user"];
     name = data["username"];
     email = data["email"];
-    rank=res["rank"];
-    points=res["points"];
+    rank = res["rank"];
+    points = res["points"].toString();
     dateJoined = DateFormat('EEEE d MMMM H:mm')
         .format(DateTime.parse(data["date_joined"]));
   }
@@ -39,99 +40,90 @@ class _ProfileState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     getProfile();
-    print(rank);
     return MaterialApp(
-        home: Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset("assets/images/LogoAsMetroName_noF.png"),
-            Container(
-              width: 100,
-              height: 100,
-              decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                border: Border.all(color: Colors.black),
-              ),
-            ),
-            SizedBox(height: 10),
-            Text.rich(
-              TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: 'Name: ', style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22
-                  )),
-                  TextSpan(text: name, style: TextStyle(
-                      fontSize: 22
-                  )),
+      home: Scaffold(
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                CircleAvatar(
+                  radius: 85,
+                  backgroundImage: AssetImage('assets/images/logo.png'),
+                ),
+                SizedBox(height: 30),
+                Text.rich(
+                  TextSpan(
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Welcome back, ',
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18)
+                      ),
+                      TextSpan(
+                          text: name,
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold, fontSize: 18)
+                      ),
+                    ],
+                  ),
 
-                ],
-              ),
+                ),
+                Card(
+                    color: Colors.white70,
+                    margin:
+                        EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.calendar_today,
+                        color: Colors.deepPurple[900],
+                      ),
+                      title: Text(
+                        dateJoined,
+                        style:
+                            TextStyle(fontWeight: FontWeight.bold, fontSize: 22,color: Colors.black),
+                      ),
+                    )),
+                Card(
+                    color: Colors.white70,
+                    margin:
+                    EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.email,
+                        color: Colors.deepPurple[900],
+                      ),
+                      title: Text(
+                        email,
+                        style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 22,color: Colors.black),
+                      ),
+                    )),
+                Card(
+                    color: Colors.white70,
+                    margin:
+                    EdgeInsets.symmetric(vertical: 5.0, horizontal: 25.0),
+                    child: ListTile(
+                      leading: Icon(
+                        Icons.star,
+                        color: Colors.deepPurple[900],
+                      ),
+                      title: Text(
+                        "Rank: "+rank+", Points: "+points,
+                        style:
+                        TextStyle(fontWeight: FontWeight.bold, fontSize: 22,color: Colors.black),
+                      ),
+                    )),
+                Button(
+                    text: "LOG OUT",
+                    press: () {
+                      widget.onLogOutPressed();
+                    })
+              ],
             ),
-            SizedBox(height: 10),
-            Text.rich(
-              TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: 'Joined: '+dateJoined, style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.grey,
-                  )),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Text.rich(
-              TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: 'E-mail: ', style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22
-                  )),
-                  TextSpan(text: email, style: TextStyle(
-                      fontSize: 22
-                  )),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Text.rich(
-              TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: 'Rank: ', style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22
-                  )),
-                  TextSpan(text: rank, style: TextStyle(
-                      fontSize: 22
-                  )),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Text.rich(
-              TextSpan(
-                children: <TextSpan>[
-                  TextSpan(text: 'Points: ', style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 22
-                  )),
-                  TextSpan(text: points.toString(), style: TextStyle(
-                      fontSize: 22
-                  )),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            TextButton(
-                onPressed: () {
-                  widget.onLogOutPressed();
-                },
-                child: Text("LOG OUT")),
-          ],
+          ),
         ),
       ),
-    ));
+    );
   }
 }
